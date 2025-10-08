d7a063b3c1056fb893a3850446e8b3ed91efcb1f
Jarek Radosz <jarek@cvx.dev>
2025-10-08 21:13:55 +0200

    DEV: Update linting

--- javascripts/discourse/initializers/init-google-fonts.js
+++ javascripts/discourse/initializers/init-google-fonts.js
@@ -1,6 +1,6 @@
 import { apiInitializer } from "discourse/lib/api";
 import GoogleFonts from "../components/google-fonts";
 
-export default apiInitializer("1.0", (api) => {
+export default apiInitializer((api) => {
   api.renderInOutlet("above-site-header", GoogleFonts);
 });
